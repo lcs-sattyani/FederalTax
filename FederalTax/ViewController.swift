2c2b851a01d92f4b7d5bb3bc462165e5212d1436
Sawsan Attyani <sattyani@lcs.on.ca>
2019-11-02 19:00:00 -0400

    completed view

--- FederalTax/ViewController.swift
+++ FederalTax/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+         //MARK: Properties
+    
+    @IBOutlet weak var NameInput: UITextField!
+    
+    @IBOutlet weak var annualIncome: UITextField!
+    
+    @IBOutlet weak var taxOwing: UILabel!
+    
+    @IBOutlet weak var taxRate: UILabel!
+    
+    
+  //MARK: Methods
+    
     override func viewDidLoad() {
+
+             
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        }
+
+    @IBAction func caculateTax(_ sender: Any) {
+        
+guard let name = (NameInput.text) else{ taxOwing.text=("Please type your name")
+            return }
+        
+        
+        
+        
+        
+        
+        
     }
-
-
+    
 }
 
